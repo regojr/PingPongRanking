a9bbe34d5f96f685601e7dcf4d3f2bbfe55eea11
Jake Rego <regojr@miamioh.edu>
2015-03-05 21:43:19 -0500

    added stuff
    working in codepen until I get SASS set up

--- public_html/js/dataprocess.js
+++ public_html/js/dataprocess.js
@@ -2,4 +2,15 @@
     Processes the necessary data for the rankings
  */
 
+function overlay() {
+	el = document.getElementById("overlay");
+	el.style.visibility = (el.style.visibility === "visible") ? "hidden" : "visible";
+}
 
+var player = ["Jake", "Rego", 999];
+
+  function initTable(one, two, three){
+    document.getElementById(one).innerHTML= player[0];
+    document.getElementById(two).innerHTML= player[1];
+    document.getElementById(three).innerHTML= player[2];
+  }
